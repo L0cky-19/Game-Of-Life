ca56c78ff687ed7a984e4929da7c2d244da4178d
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2024-12-03 16:14:30 +0100

    COMMENTAIRES SUIVI

--- src/Game.cpp
+++ src/Game.cpp
@@ -19,7 +19,7 @@ using namespace std;
  * 4. Iteration delay
  * todo: nb of iterations
  */
-void Game::setup()
+void Game::setup()  //!todo: add class or create game methods to separate this data?
 {
 
     // todo maybe make this kind of class? UserConfig config = getUserInput();
@@ -81,6 +81,7 @@ void Game::setup()
     cout << "Iteration Delay: " << iterationDelay << " ms\n";
 
     // set the new values
+    //TODO: use stoi as iterationDelay is an int
     this->setIterationDelay(static_cast<float>(iterationDelay)); // Assuming iterationDelay is a float
     if (evolutionStrategy == 0) {
         this->setEvolutionStrategy(new ClassicEvolution()); // Assuming ClassicEvolution is defined
@@ -93,7 +94,7 @@ void Game::setup()
     } else {
         this->setRenderer(new GraphicRenderer()); // Assuming GraphicRenderer is defined
     }
-
+    //TODO: subscribe?
 
     /*
     grid = new Grid(config.size);
@@ -117,7 +118,7 @@ void Game::run()
     {
         grid->calculateNextGen();
         renderer->update();
-        fileHandler->saveGridToFile();
+        fileHandler->saveGridToFile(); //TODO: Make fileHandler subscribe to the update() event also?? Rename to notify maybe?
 
         if (isPaused)
         {
@@ -126,7 +127,8 @@ void Game::run()
     }*/
 }
 
-
+//TODO: for pause use multithreading??? a second thread for the console cin so that if the user gives a pause input. We use an IPC (inter processing ...) and it tells the other thread to stop.
+//TODO: l'implémenter a la fin
 void Game::pause() //TODO: pas logique d'avoir ces deux fonctions qui font la meme chose.
 {
     currentState->update();
