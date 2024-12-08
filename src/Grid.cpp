00df018ddc956e0ed4e38af528958286d7cb2bbc
Hexa <84851013+ctrl-hexa@users.noreply.github.com>
2024-12-08 14:21:45 +0100

    chore:refac

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -13,21 +13,14 @@ Grid::Grid(int width, int height) : width(width), height(height) {}
 
 void Grid::initCells(const std::vector<std::vector<int>> &tab) { // FIXME: si on a plus de celules que dites ca va crash
     std::vector<std::vector<Cell>> newCells = {};
-    for (int i = 0; i < height; i++)
-    {
+    for (int i = 0; i < height; i++) {
         std::vector<Cell> newRow = {};
-        for (int y = 0; y < width; y++)
-        {
-            if (tab[i][y] == 1)
-            {
+        for (int y = 0; y < width; y++) {
+            if (tab[i][y] == 1) {
                 newRow.push_back(Cell(CellType::Alive));
-            }
-            else if (tab[i][y] == 2)
-            {
+            } else if (tab[i][y] == 2) {
                 newRow.push_back(Cell(CellType::Obstacle));
-            }
-            else
-            {
+            } else {
                 newRow.push_back(Cell(CellType::Dead));
             }
         }
@@ -50,15 +43,13 @@ void Grid::initCellsRandom()
     for (int y = 0; y < height; y++)
     {
         std::vector<Cell> newRow = {};
-        for (int x = 0; x < width; x++)
-        {
+        for (int x = 0; x < width; x++) {
             newRow.push_back(rand() % 2 == 0 ? CellType::Dead : CellType::Alive);
         }
         newCells.push_back(newRow);
     }
 
-    for (int i = 0; i < numObstacles; i++)
-    {
+    for (int i = 0; i < numObstacles; i++) {
         int randY = rand() % height;
         int randX = rand() % width;
         newCells[randY][randX] = Cell(CellType::Obstacle);
@@ -67,31 +58,23 @@ void Grid::initCellsRandom()
 }
 
 
-int Grid::countLiveNeighbors(int x, int y)
-{
+int Grid::countLiveNeighbors(int x, int y) {
     int count = 0;
-    for (int dy = -1; dy <= 1; dy++)
-    {
-        for (int dx = -1; dx <= 1; dx++)
-        {
+    for (int dy = -1; dy <= 1; dy++) {
+        for (int dx = -1; dx <= 1; dx++) {
             if (dx == 0 && dy == 0)
                 continue;
 
             int nx = x + dx;
             int ny = y + dy;
 
-            if (isToroidal)
-            {
+            if (isToroidal) {
                 nx = (nx + width) % width;
                 ny = (ny + height) % height;
-            }
-            else if (nx < 0 || nx >= width || ny < 0 || ny >= height)
-            {
+            } else if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
                 continue;
             }
-
-            if (cells[ny][nx].getType() == CellType::Alive)
-            {
+            if (cells[ny][nx].getType() == CellType::Alive) {
                 count++;
             }
         }
@@ -99,14 +82,10 @@ int Grid::countLiveNeighbors(int x, int y)
     return count;
 }
 
-bool Grid::isGridStable(const vector<vector<CellType>> &nextGen) const
-{
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            if (nextGen[y][x] != cells[y][x].getType())
-            {
+bool Grid::isGridStable(const vector<vector<CellType>> &nextGen) const {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            if (nextGen[y][x] != cells[y][x].getType()) {
                 return false;
             }
         }
@@ -121,19 +100,13 @@ bool Grid::calculateNextGen(IEvolutionStrategy *evolutionStrategy)
     threadCount = threadCount > 0 ? threadCount : 4; // Fallback to 4 threads if detection fails
 
     auto processChunk = [&](int startRow, int endRow) {
-        for (int y = startRow; y < endRow; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                if (cells[y][x].getType() == CellType::Obstacle)
-                {
+        for (int y = startRow; y < endRow; y++) {
+            for (int x = 0; x < width; x++) {
+                if (cells[y][x].getType() == CellType::Obstacle) {
                     nextGen[y][x] = CellType::Obstacle;
-                }
-                else
-                {
+                } else {
                     int neighbors = countLiveNeighbors(x, y);
-                    if (evolutionStrategy->evolve(&cells[y][x], neighbors))
-                    {
+                    if (evolutionStrategy->evolve(&cells[y][x], neighbors)) {
                         nextGen[y][x] = CellType::Alive;
                     }
                 }
@@ -147,8 +120,7 @@ bool Grid::calculateNextGen(IEvolutionStrategy *evolutionStrategy)
     int remainingRows = height % threadCount;
 
     int currentRow = 0;
-    for (int i = 0; i < threadCount; ++i)
-    {
+    for (int i = 0; i < threadCount; ++i) {
         int startRow = currentRow;
         int endRow = startRow + rowsPerThread + (remainingRows-- > 0 ? 1 : 0);
         threads.emplace_back(processChunk, startRow, endRow);
@@ -156,19 +128,15 @@ bool Grid::calculateNextGen(IEvolutionStrategy *evolutionStrategy)
     }
 
     // Join threads
-    for (auto &thread : threads)
-    {
+    for (auto &thread : threads) {
         if (thread.joinable())
             thread.join();
     }
-
-    if (isGridStable(nextGen))
-    {
+    if (isGridStable(nextGen)) {
         return true;
     }
 
-    for (int y = 0; y < height; y++)
-    {
+    for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++)
         {
             cells[y][x].setType(nextGen[y][x]);
